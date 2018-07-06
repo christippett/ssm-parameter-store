0e0f4ad224f96fa8717048b4bbf9e95816ace9fa
Chris Tippett <c.tippett@gmail.com>
2018-07-06 19:30:55 +1000

    Add method to get single parameter, refactor others

--- ssm_parameter_store/stores.py
+++ ssm_parameter_store/stores.py
@@ -9,45 +9,52 @@ logger = logging.getLogger(__name__)
 
 
 class EC2ParameterStore:
-    def __init__(self, path_delimiter='/'):
+    def __init__(self):
         self.client = boto3.client('ssm')
-        self.path_delimiter = path_delimiter
+        self.path_delimiter = '/'
 
     @classmethod
     def set_env(cls, parameter_dict):
         for k, v in parameter_dict.items():
             os.environ.setdefault(k, v)
 
-    def _extract_parameter(self, parameter):
-        name = parameter['Name']
-        name_parts = name.split(self.path_delimiter)
-        key = name_parts[-1]
-        value = parameter['Value']
-        return (key, value)
-
-    def get_parameter(self, name, decrypt=True):
-        result = self.client.get_parameter(Name=name, WithDecryption=decrypt)
-        p = result['Parameter']
-        return dict([self._extract_parameter(p)])
-
-    def get_parameters_by_path(self, path, decrypt=True, recursive=True):
+    def _get_paginated_parameters(self, client_method, strip_path=True, **client_kwargs):
         next_token = None
-        client_kwargs = dict(Path=path, WithDecryption=decrypt, Recursive=recursive)
         parameters = []
         while True:
-            result = self.client.get_parameters_by_path(**client_kwargs)
+            result = client_method(**client_kwargs)
             parameters += result.get('Parameters')
             next_token = result.get('NextToken')
             if next_token is None:
                 break
             client_kwargs.update({'NextToken': next_token})
-        return dict(self._extract_parameter(p) for p in parameters)
+        return dict(self.extract_parameter(p, strip_path=strip_path) for p in parameters)
 
+    def extract_parameter(self, parameter, strip_path=False):
+        key = parameter['Name']
+        if strip_path:
+            key_parts = key.split(self.path_delimiter)
+            key = key_parts[-1]
+        value = parameter['Value']
+        return (key, value)
 
-if __name__ == '__main__':
-    store = EC2ParameterStore()
-    parameters = store.get_parameters_by_path(path='/aeroster/common')
-    print(json.dumps(parameters))
-    EC2ParameterStore.set_env(parameters)
-    # print(os.environ.get('MAILGUN_API_KEY'))
-    # print(store.get_parameter('/aeroster/common/AWS_S3_CUSTOM_DOMAIN'))
+    def get_parameter(self, name, decrypt=True, strip_path=True):
+        result = self.client.get_parameter(Name=name, WithDecryption=decrypt)
+        p = result['Parameter']
+        return dict([self.extract_parameter(p, strip_path=strip_path)])
+
+    def get_parameters(self, names, decrypt=True, strip_path=True):
+        client_kwargs = dict(Names=names, WithDecryption=decrypt)
+        return self._get_paginated_parameters(
+            client_method=self.client.get_parameters,
+            strip_path=strip_path,
+            **client_kwargs
+        )
+
+    def get_parameters_by_path(self, path, decrypt=True, recursive=True, strip_path=True):
+        client_kwargs = dict(Path=path, WithDecryption=decrypt, Recursive=recursive)
+        return self._get_paginated_parameters(
+            client_method=self.client.get_parameters_by_path,
+            strip_path=strip_path,
+            **client_kwargs
+        )
