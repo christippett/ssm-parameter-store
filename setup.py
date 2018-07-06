12423e7e591e1e5b7dfd1535dc87e45092426a8e
Chris Tippett <c.tippett@gmail.com>
2018-07-07 00:11:57 +1000

    Reorganise repository, add src/ directory

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
 
 setup(
     name='ssm-parameter-store',
@@ -6,24 +7,20 @@ setup(
     description='Python wrapper for storing secrets in AWS Systems Manager Parameter Store',
     url='http://github.com/christippett/ssm-parameter-store',
     author='Chris Tippett',
-    author_email='c.tippett@gmail.com',
+    author_email='chris@spoon.nz',
     license='MIT',
-    packages=['ssm_parameter_store'],
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
     classifiers=[
-        'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 1.7',
-        'Framework :: Django :: 1.8',
-        'Framework :: Django :: 1.9',
-        'Framework :: Django :: 1.10',
-        'Framework :: Django :: 1.11',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     zip_safe=False
 )
