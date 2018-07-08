dd0b026881151e30e61c3dd120032f5b07a7a42c
Chris Tippett <c.tippett@gmail.com>
2018-07-08 15:38:55 +1000

    Update setup.py to read description from README.rst

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
 
+LONG_DESCRIPTION = open('README.rst').read()
+
+
 setup(
     name='ssm-parameter-store',
     version='0.1',
-    description='Python wrapper for storing secrets in AWS Systems Manager Parameter Store',
+    description='Simple Python wrapper for getting values from AWS Systems Manager Parameter Store',
+    long_description=LONG_DESCRIPTION,
     url='http://github.com/christippett/ssm-parameter-store',
     author='Chris Tippett',
     author_email='chris@spoon.nz',
@@ -14,13 +18,15 @@ setup(
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
+        'Operating System :: OS Independent',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
     zip_safe=False
 )
