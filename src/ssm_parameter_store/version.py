9d84d4f2c5fbfe3d11c88ae5cb43578559cb3759
Chris Tippett <c.tippett@gmail.com>
2018-07-08 22:13:59 +1000

    Properly resolve version in package

--- src/ssm_parameter_store/version.py
+++ src/ssm_parameter_store/version.py
@@ -2,6 +2,6 @@ from pkg_resources import get_distribution, DistributionNotFound
 
 
 try:
-    __version__ = get_distribution(__name__).version
+    __version__ = get_distribution('ssm-parameter-store').version
 except DistributionNotFound:
     __version__ = 'unknown'  # package not installed
