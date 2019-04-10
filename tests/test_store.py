c5a5e0b283e387e42927475e7e7af2b26c0e9896
Gary Donovan <gary.donovan@rockmelon.com>
2019-04-11 09:41:56 +1000

    Make tests work with current versions of py.test
    * Inject test fixture, don't call it
    * Shoehorn in a single mock_ssm per test case

--- tests/test_store.py
+++ tests/test_store.py
@@ -6,7 +6,15 @@ from ssm_parameter_store import EC2ParameterStore
 
 
 @pytest.fixture
-def parameter_store():
+def fake_ssm():
+    """Provide a consistent faked SSM context for running an entire test case."""
+    with mock_ssm():
+        yield None
+
+
+@pytest.fixture
+def parameter_store(fake_ssm):
+    """An SSM parameter store with a well-known set of fake test data"""
     # Load test parameters
     ssm = boto3.client('ssm')
     ssm.put_parameter(Name='key', Value='hello', Type='SecureString')
@@ -20,47 +28,39 @@ def parameter_store():
     return EC2ParameterStore()
 
 
-@mock_ssm
-def test_extract_parameter_returns_key_pair_tuple():
-    store = parameter_store()
+def test_extract_parameter_returns_key_pair_tuple(parameter_store):
     parameter = {
         'Name': 'key',
         'Value': 'hello'
     }
-    extracted_parameter = store.extract_parameter(parameter)
+    extracted_parameter = parameter_store.extract_parameter(parameter)
     assert isinstance(extracted_parameter, tuple)
     assert extracted_parameter[0] == 'key'
     assert extracted_parameter[1] == 'hello'
 
 
-@mock_ssm
-def test_get_paginated_parameters():
+def test_get_paginated_parameters(parameter_store):
     """ SSM returns a maximum of 10 parameters per response
         - additional keys can be requested by passing
         NextToken in subsequent requests.
     """
-    store = parameter_store()
     client_kwargs = dict(Path='/test/path/')
-    parameter_keys = store._get_paginated_parameters(
+    parameter_keys = parameter_store._get_paginated_parameters(
         client_method=boto3.client('ssm').get_parameters_by_path,
         **client_kwargs
     )
     assert len(parameter_keys) == 20
 
 
-@mock_ssm
-def test_get_parameter():
-    store = parameter_store()
-    parameter_keys = store.get_parameter('key')
+def test_get_parameter(parameter_store):
+    parameter_keys = parameter_store.get_parameter('key')
     assert len(parameter_keys) == 1
     assert 'key' in parameter_keys.keys()
     assert parameter_keys.get('key') == 'hello'
 
 
-@mock_ssm
-def test_get_parameters():
-    store = parameter_store()
-    parameter_keys = store.get_parameters(['key', 'second-key'])
+def test_get_parameters(parameter_store):
+    parameter_keys = parameter_store.get_parameters(['key', 'second-key'])
     assert len(parameter_keys) == 2
     assert 'key' in parameter_keys.keys()
     assert 'second-key' in parameter_keys.keys()
@@ -68,36 +68,27 @@ def test_get_parameters():
     assert parameter_keys.get('second-key') == 'world'
 
 
-@mock_ssm
-def test_get_parameters_by_path():
-    store = parameter_store()
-    parameter_keys = store.get_parameters_by_path('/path/to/another/key/')
+def test_get_parameters_by_path(parameter_store):
+    parameter_keys = parameter_store.get_parameters_by_path('/path/to/another/key/')
     assert len(parameter_keys) == 1
 
 
-@mock_ssm
-def test_get_parameters_by_path_with_recursion():
-    store = parameter_store()
-    parameter_keys = store.get_parameters_by_path('/path/to/', recursive=True)
+def test_get_parameters_by_path_with_recursion(parameter_store):
+    parameter_keys = parameter_store.get_parameters_by_path('/path/to/', recursive=True)
     assert len(parameter_keys) == 3
 
 
-@mock_ssm
-def test_get_parameters_by_path_without_recursion():
-    store = parameter_store()
-    parameter_keys = store.get_parameters_by_path('/path/to/', recursive=False)
+def test_get_parameters_by_path_without_recursion(parameter_store):
+    parameter_keys = parameter_store.get_parameters_by_path('/path/to/', recursive=False)
     assert len(parameter_keys) == 2
 
 
-@mock_ssm
-def test_stripping_path_in_parameter():
-    store = parameter_store()
-    parameter_keys = store.get_parameter('/path/to/third-key', strip_path=False)
+def test_stripping_path_in_parameter(parameter_store):
+    parameter_keys = parameter_store.get_parameter('/path/to/third-key', strip_path=False)
     assert parameter_keys.get('/path/to/third-key')
 
-    stripped_parameter_keys = store.get_parameter('/path/to/third-key', strip_path=True)
+    stripped_parameter_keys = parameter_store.get_parameter('/path/to/third-key', strip_path=True)
     assert stripped_parameter_keys.get('third-key')
 
-    stripped_parameter_keys = store.get_parameter('key', strip_path=True)
+    stripped_parameter_keys = parameter_store.get_parameter('key', strip_path=True)
     assert stripped_parameter_keys.get('key')
-
